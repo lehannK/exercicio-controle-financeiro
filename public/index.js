5cb1b025e4d576eb7304d82717cf0a5730bf1f03
leandro martins <leandro_hollies@hotmail.com>
2023-09-16 22:18:27 -0300

    Revert "merge-ok"
    This reverts commit e24a4047a537f803104514d40c27d1fe7bbd4b66, reversing
    changes made to 302e5c37c060c7478db19f413249ee1bf71e83c1.

--- public/index.js
+++ public/index.js
@@ -6,9 +6,6 @@ export const valueTransaction = document.getElementById("value-transaction");
 export const btnSendValue = document.getElementById("btn-send-value");
 export const transactionList = document.getElementById("transaction-list");
 export const btnShowExtract = document.getElementById("show-extract");
-export const showTotalValueDiv = document.getElementById(
-  "show-total-value-div"
-);
 
 btnSendValue.addEventListener("click", postRequest);
 btnShowExtract.addEventListener("click", showTransactions);
